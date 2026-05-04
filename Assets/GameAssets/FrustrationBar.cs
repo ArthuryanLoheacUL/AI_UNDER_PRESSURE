50478277bc53237f9eee4d265460d17dad5f2c2a
Arthuryan Loheac <arthuryanloheac@umeshulovers.com>
2026-05-04 17:24:44 +0200

    [Feat] Add Shake Frustration bar

--- Assets/GameAssets/FrustrationBar.cs
+++ Assets/GameAssets/FrustrationBar.cs
@@ -11,12 +11,31 @@ public class FrustrationBar : MonoBehaviour
 
     bool isUpping = false;
 
+    // Shake variables
+    private bool isShaking = false;
+    private float shakeDuration = 0.3f;
+    private float shakeIntensity = 5f;
+    private float shakeTimer = 0f;
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = transform.localPosition;
+    }
+
+    public void TriggerShake()
+    {
+        isShaking = true;
+        shakeTimer = shakeDuration;
+    }
+
     public void SetFrustration(float frustration)
     {
         targetFrustration = frustration;
         if (targetFrustration > sliderFG.value)
         {
             isUpping = true;
+            TriggerShake(); // Shake when frustration increases
         }
         else
         {
@@ -78,6 +97,24 @@ public class FrustrationBar : MonoBehaviour
                 if (sliderBG.value < targetFrustration)
                     sliderBG.value = targetFrustration;
             }  
+        }
+
+        // Apply shake effect
+        if (isShaking)
+        {
+            shakeTimer -= Time.deltaTime;
+            if (shakeTimer <= 0f)
+            {
+                isShaking = false;
+                transform.localPosition = originalPosition;
+            }
+            else
+            {
+                float shakeAmount = shakeIntensity * (shakeTimer / shakeDuration);
+                float shakeX = Random.Range(-shakeAmount, shakeAmount);
+                float shakeY = Random.Range(-shakeAmount, shakeAmount);
+                transform.localPosition = originalPosition + new Vector3(shakeX, shakeY, 0f);
+            }
         }
     }
 }
