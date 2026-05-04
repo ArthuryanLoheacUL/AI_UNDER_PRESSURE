e9de8d0d6ecb34c4cd162d4aad446ec07fe27312
Arthuryan Loheac <arthuryanloheac@umeshulovers.com>
2026-05-04 13:12:39 +0200

    [Feat] Color change button

--- Assets/GameAssets/ButtonsResponse/ButtonResponseUI.cs
+++ Assets/GameAssets/ButtonsResponse/ButtonResponseUI.cs
@@ -1,11 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonResponseUI : MonoBehaviour
 {
     public TMP_Text responseText;
     public GameObject valueIndicator;
+    public RawImage valueIndicatorSprite;
     public TMP_Text valueIndicatorText;
+    public Color colorNegative;
 
     public void SetResponse(string response, int value)
     {
@@ -15,6 +18,11 @@ public class ButtonResponseUI : MonoBehaviour
         {
             valueIndicator.SetActive(true);
             valueIndicatorText.text = value > 0 ? $"+{value}" : value.ToString();
+            if (value < 0)
+            {
+                valueIndicatorText.color = colorNegative;
+                valueIndicatorSprite.color = colorNegative;
+            }
         }
         else
         {
